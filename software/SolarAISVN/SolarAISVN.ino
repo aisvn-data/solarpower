fd8dbf7d7fd572fba0f0bc60deb620b9c162ae7c
aisvn-data <65865481+aisvn-data@users.noreply.github.com>
2020-11-18 20:56:17 +0700

    initial commit May 2020

---
+++ software/SolarAISVN/SolarAISVN.ino
@@ -0,0 +1,124 @@
+// Solarmeter first attempt
+// Inspired by https://randomnerdtutorials.com/esp32-esp8266-publish-sensor-readings-to-google-sheets/
+ 
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+
+#include <Wire.h>
+
+// Replace with your SSID and Password
+const char* ssid     = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Replace with your unique IFTTT URL resource
+const char* resource = "/trigger/solar/with/key/nAZjOphL3d-ZO4N3k64-1A7gTlNSrxMJdmqy3";
+
+// Maker Webhooks IFTTT
+const char* server = "maker.ifttt.com";
+
+// Time to sleep
+uint64_t uS_TO_S_FACTOR = 1000000;  // Conversion factor for micro seconds to seconds
+// sleep for 30 minutes = 1800 seconds
+uint64_t TIME_TO_SLEEP = 120;
+
+int adcValue = 0;
+
+void setup() {
+  Serial.begin(115200); 
+  delay(2000);
+
+  initWifi();
+  makeIFTTTRequest();
+    
+  #ifdef ESP32
+    // enable timer deep sleep
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);    
+    Serial.println("Going to sleep now");
+    // start deep sleep for 120 seconds (2 minutes)
+    esp_deep_sleep_start();
+  #else
+    // Deep sleep mode for 120 seconds (2 minutes)
+    Serial.println("Going to sleep now");
+    ESP.deepSleep(TIME_TO_SLEEP * uS_TO_S_FACTOR); 
+  #endif
+}
+
+void loop() {
+  // sleeping so wont get here 
+}
+
+// Establish a Wi-Fi connection with your router
+void initWifi() {
+  Serial.print("Connecting to: "); 
+  Serial.print(ssid);
+  WiFi.begin(ssid, password);  
+
+  int timeout = 10 * 4; // 10 seconds
+  while(WiFi.status() != WL_CONNECTED  && (timeout-- > 0)) {
+    delay(250);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  if(WiFi.status() != WL_CONNECTED) {
+     Serial.println("Failed to connect, going back to sleep");
+  }
+
+  Serial.print("WiFi connected in: "); 
+  Serial.print(millis());
+  Serial.print(", IP address: "); 
+  Serial.println(WiFi.localIP());
+}
+
+// Make an HTTP request to the IFTTT web service
+void makeIFTTTRequest() {
+  Serial.print("Connecting to "); 
+  Serial.print(server);
+  
+  WiFiClient client;
+  int retries = 5;
+  while(!!!client.connect(server, 80) && (retries-- > 0)) {
+    Serial.print(".");
+  }
+  Serial.println();
+  if(!!!client.connected()) {
+    Serial.println("Failed to connect...");
+  }
+  
+  Serial.print("Request resource: "); 
+  Serial.println(resource);
+
+  // raw and converted voltage reading
+  adcValue = analogRead( 34 );
+  String jsonObject = String("{\"value1\":\"") + adcValue + "\",\"value2\":\"" + (adcValue * 2.4)
+                      + "\",\"value3\":\"" + millis() + "\"}";
+                      
+  // Comment the previous line and uncomment the next line to publish temperature readings in Fahrenheit                    
+  /*String jsonObject = String("{\"value1\":\"") + (1.8 * bme.readTemperature() + 32) + "\",\"value2\":\"" 
+                      + (bme.readPressure()/100.0F) + "\",\"value3\":\"" + bme.readHumidity() + "\"}";*/
+                      
+  client.println(String("POST ") + resource + " HTTP/1.1");
+  client.println(String("Host: ") + server); 
+  client.println("Connection: close\r\nContent-Type: application/json");
+  client.print("Content-Length: ");
+  client.println(jsonObject.length());
+  client.println();
+  client.println(jsonObject);
+        
+  int timeout = 5 * 10; // 5 seconds             
+  while(!!!client.available() && (timeout-- > 0)){
+    delay(100);
+  }
+  if(!!!client.available()) {
+    Serial.println("No response...");
+  }
+  while(client.available()){
+    Serial.write(client.read());
+  }
+  
+  Serial.println("\nclosing connection");
+  client.stop(); 
+}
